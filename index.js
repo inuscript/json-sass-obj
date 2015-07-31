321224dbc28823aa18a0bf1d112feec844c4d6a7
inuscript <inuscript@gmail.com>
2015-07-31 23:26:01 +0900

    split json-sass-obj

---
+++ index.js
@@ -0,0 +1,15 @@
+var jsToSassString = require('json-sass/lib/jsToSassString') // TODO: Fix if json-sass bug
+var through2 = require("through2")
+var replaceExt = require('replace-ext');
+
+module.exports = function(options){
+  return through2.obj(function(file, enc, cb){
+    var jsValue = JSON.parse(file.contents.toString())
+    var sassString = jsToSassString(jsValue)
+    sassString = options.prefix + sassString + options.suffix;
+    file.contents = new Buffer(sassString)
+    file.path = replaceExt(file.path, ".scss")
+    this.push(file)
+    cb()
+  })
+}
