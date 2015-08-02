fedf0d1172c3902885e1007c16f453b8b71ab858
inuscript <inuscript@gmail.com>
2015-08-02 19:04:50 +0900

    test2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// var jsToSassString = require('json-sass/lib/jsToSassString') // TODO: Fix if json-sass bug
+var jsToSassString = require('json-sass/lib/jsToSassString') // TODO: Fix if json-sass bug
 var through2 = require("through2")
 var replaceExt = require('replace-ext');
 
 module.exports = function(options){
   return through2.obj(function(file, enc, cb){
     var jsValue = JSON.parse(file.contents.toString())
-    var sassString = "" //jsToSassString(jsValue)
-    sassString = options.prefix + sassString + options.suffix;
+    var sassString = jsToSassString(jsValue)
+    // sassString = options.prefix + sassString + options.suffix;
     file.contents = new Buffer(sassString)
     if(typeof file.path === "string"){
       file.path = replaceExt(file.path, ".scss")
