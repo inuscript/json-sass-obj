c4e475cd84d80af2fe8344f853f9b57528f03f7b
inuscript <inuscript@gmail.com>
2015-08-02 16:49:06 +0900

    add test

---
+++ README.md
@@ -0,0 +1,3 @@
+# json-sass-obj
+
+Object mode wrapper for [jsonSass](https://github.com/acdlite/json-sass)
---
+++ test/index.js
@@ -0,0 +1,27 @@
+var assert = require("power-assert") 
+var File = require('vinyl');
+var jsonSassObj = require("../")
+var fs = require("fs")
+
+describe("json-sass-obj", function(){
+  it("should return sass file object", function(done){
+    var fakeFile = new File({
+      path: "path/to/some.json",
+      contents: new Buffer(JSON.stringify({
+        "foo" : "baz",
+        "bee" : [1,2,3]
+      }))
+    })
+    var stream = jsonSassObj({
+      prefix: "$someItem: ",
+      suffix: " !default;"
+    })
+    stream.write(fakeFile)
+    stream.once('data', function(file){
+      var scss = file.contents.toString()
+      assert.equal(scss, fs.readFileSync("./test/fixture/map.scss"))
+      assert.equal(file.path, "path/to/some.scss")
+      done()
+    })
+  })
+})
